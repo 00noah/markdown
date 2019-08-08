337dda361911fe2e5584c455da9a001eae85f875
Dustin Effinger and Noah Horwitz <dustin@>
2019-08-07 19:21:44 -0700

    our recipe

---
+++ markdown.mk
@@ -0,0 +1,29 @@
+# My Muffins
+
+![muffin](http://lorempixel.com/400/200/)
+
+My muffin's bring all the coders to the yard.
+My grandpa always used to say this about this recipe:
+> "it's ok."
+
+
+## Ingredients
+-----------------------
+
+
+* eggs
+* flour
+* milk
+* blueberries
+* salt
+* sugar
+* _love_
+
+## Directions
+
+1. mix everything together
+2. bake for one year at 425
+3. **after** fire is put out, remove and enjoy!
+
+[click here](https://google.com) to enjoy my other recipes!
+\* warning, don't try this at home.
